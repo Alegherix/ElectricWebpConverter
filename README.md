182eeea00d0b6b000548fde21618817439f64e57
Alegherix <psao69@gmail.com>
2020-11-24 21:13:05 +0100

    inital commit

---
+++ README.md
@@ -0,0 +1,8 @@
+### Installation
+
+1. Ladda ner Node och installera -> https://nodejs.org/en/
+2. Ladda ner antigen NPM eller Yarn
+3. Clonea repot
+4. npm install / yarn install (Baserat på om ni kör npm eller yarn)-> För att installera dependencies
+5. Lägg alla .png bilder i assets
+6. I terminalen, kör npm run start / yarn start (Baserat på om ni kör npm eller yarn)
---
+++ index.js
@@ -0,0 +1,23 @@
+const webp = require('webp-converter');
+const fs = require('fs');
+
+// Returnerar filnamn
+const getImgName = (imgPath) => {
+  return imgPath.split('.png')[0];
+};
+
+const imgPaths = [];
+fs.readdirSync('./assets').forEach((file) => {
+  console.log(file);
+  imgPaths.push(file);
+});
+
+imgPaths.forEach((img) => {
+  const name = getImgName(img);
+  const quality = 80;
+  webp.cwebp(
+    `./assets/${name}.png`,
+    `./convertedImages/${name}.webp`,
+    `-q ${quality}`
+  );
+});
