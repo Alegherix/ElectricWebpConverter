ad1eada572df7cb578c611aa6e9bb671455842c5
Alegherix <psao69@gmail.com>
2020-11-25 06:30:48 +0100

    updated to only use Sharp, no webp-converter

--- README.md
+++ README.md
@@ -4,8 +4,4 @@
 2. Clonea repot och starta en terminal i ElectricWebpConverter
 3. I terminalen kör 'npm install' -> utan ''
 4. Lägg alla .png bilder att konvertera i assets foldern(bara .png, inga andra filer)
-5. I terminalen, kör 'npm run start' -> utan ''
-
-### Multipla Storlekar
-
-1. För att skapa bilder i multipla storlekar -> I terminalen EFTER ni kört npm run start och fått alla bilder i convertedImages, avbryt processen om nodemon fortfarande körs och kör nu 'npm run multiple'
+5. I terminalen, kör antigen, "node index.js" för att bara konvertera orginalbilden, ELLER "node index.js -m" för att konvertera orginalbilden och få ut multipla storlekar
--- index.js
+++ index.js
@@ -1,21 +1,46 @@
-const webp = require('webp-converter');
 const fs = require('fs');
+const sharp = require('sharp');
 
-// Returnerar filnamn
+// Parses CLI
+const args = process.argv.slice(2);
+
+// Utility function
 const getImgName = (imgPath) => {
   return imgPath.split('.png')[0];
 };
-const imgPaths = [];
-fs.readdirSync('./assets').forEach((file) => {
-  imgPaths.push(file);
-});
 
-imgPaths.forEach((img) => {
-  const name = getImgName(img);
-  const quality = 80;
-  webp.cwebp(
-    `./assets/${name}.png`,
-    `./convertedImages/${name}.webp`,
-    `-q ${quality}`
-  );
-});
+// Converts Original img to .webp
+function convertOriginal() {
+  fs.readdirSync('./assets').forEach((file) => {
+    const name = getImgName(file);
+    sharp(`./assets/${name}.png`)
+      .toFormat('webp')
+      .webp({ quality: 100 })
+      .toFile(`./convertedImages/${name}.webp`);
+  });
+}
+
+// Convert img to new dimension
+function convertToDifferentSizes(width, suffix, imgName) {
+  const imgNameNoExt = getImgName(imgName);
+  sharp(`./assets/${imgNameNoExt}.png`)
+    .resize(width)
+    .toFormat('webp')
+    .webp({ quality: 100 })
+    .toFile(`./convertedImages/${imgNameNoExt}${suffix}.webp`);
+}
+
+// // Get multiple sizes, feel free to change dimensions to suit your needs.
+function getMultipleSizes() {
+  fs.readdirSync('./assets').forEach((file) => {
+    convertToDifferentSizes(480, '_S', file);
+    convertToDifferentSizes(768, '_M', file);
+    convertToDifferentSizes(1024, '_L', file);
+    convertToDifferentSizes(1440, '_XL', file);
+  });
+}
+
+convertOriginal();
+if (args[0] !== null && args[0] === '-m') {
+  getMultipleSizes();
+}
